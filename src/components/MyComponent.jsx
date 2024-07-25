9236e9071ba7b2e2c8dada183fd46b0f3d0dd90c
ngophuc29 <ngophuc2911@gmail.com>
2024-07-25 15:41:18 +0700

    first Commit Learn About Component

---
+++ src/components/MyComponent.jsx
@@ -0,0 +1,21 @@
+// 2 cach de dinh nghia 1 component
+// 1 class component
+// 2 function component
+
+import React from "react";
+// 1 class component
+class MyComponent extends React.Component {
+    // jsx
+    render() {
+
+        return(
+
+            <div>
+                My first Component
+            </div>
+        );
+    }
+
+}
+export default MyComponent;
+ 
