6f1cda9d4fbe7cf0ae3be5f61b05ef370fce0e64
ngophuc29 <ngophuc2911@gmail.com>
2024-08-06 10:31:10 +0700

    #53.1 chinh header va phan content

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import {NavLink, Link } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 
 const Header = () => {
     return (
@@ -19,20 +19,22 @@ const Header = () => {
                         <Nav.Link href="/admin">Admin</Nav.Link> */}
 
                         <NavLink to="/" className='nav-link'>Home</NavLink>
-                        <NavLink to="/user"  className='nav-link'>User</NavLink>
+                        <NavLink to="/user" className='nav-link'>User</NavLink>
 
-                        <NavLink to="/admin"  className='nav-link'>Admin</NavLink>
+                        <NavLink to="/admin" className='nav-link'>Admin</NavLink>
 
                     </Nav>
                     <Nav>
-                    <NavDropdown title="Settings" id="basic-nav-dropdown">
+                        <button className='btn-login'>Login</button>
+                        <button className='btn-signup'>Sign up</button>
+                        {/* <NavDropdown title="Settings" id="basic-nav-dropdown">
                             <NavDropdown.Item  >Login</NavDropdown.Item>
                             <NavDropdown.Item  >
                                 Logout
                             </NavDropdown.Item>
                             <NavDropdown.Item  >User Info</NavDropdown.Item>
-                           
-                        </NavDropdown>
+
+                        </NavDropdown> */}
                     </Nav>
                 </Navbar.Collapse>
             </Container>
--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,9 +1,21 @@
 import videoHomepage from "../../assests/video-homepage.mp4"
 const HomePage = () => {
     return (<div className="homepage-container">
-        <video   autoPlay muted loop>
+        <video autoPlay muted loop>
             <source src={videoHomepage} type="" />
         </video>
+
+        <div className="homepage-content"       >
+            <div className="title1">There's a better way to ask</div>
+            <div className="title2">Collect more and better data. Embed forms where people see them, from web to email.
+                Ask the right follow-up question at the right time to reveal deeper insights.</div>
+            <div className="title3">
+                <button>
+                    Get started is free
+                </button>
+            </div>
+
+        </div>
     </div>
     );
 }
